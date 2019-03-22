4ba2dfbec5f89b443e77c60394e906ea1e044ebb
Daniel Hoherd <daniel.hoherd@gmail.com>
2019-03-21 17:48:41 -0700

    Report on count of current result and report last result when result changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
+
+log_message() {
+  printf "%s %s\n" "$(date "+%F %T%z")" "$*" | tee -a "wan_connection-$(date +%F).log"
+}
+
+log_message "Starting up logger"
+
 while true ; do
-  result="$(fping -t 1000 -q 4.2.2.1 && echo "alive $(dig +short myip.opendns.com @resolver1.opendns.com)" || echo FAILURE)"
-  printf "%s %s\n" "$(date "+%F %T%z")" "${result}" | tee -a "wan_connection-$(date +%F).log"
+  this_result="$(fping -t 1000 -q 4.2.2.1 && echo "alive $(dig +short myip.opendns.com @resolver1.opendns.com)" || echo FAILURE)"
+  if [ "${last_result}" != "${this_result}" ] ; then
+    [[ ! -z "${count}" ]] && flap_message=" (last_result: ${last_result} ${count})"
+    count=0
+  fi
+  last_result="${this_result}"
+  (( count+=1 ))
+  info_message="${this_result} ${count}${flap_message}"
+  log_message "${info_message}"
   sleep 10
 done
